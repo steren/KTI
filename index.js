e4f690848a58073f0253f4c743a23f6856d53b55
Steren <steren.giannini@gmail.com>
2023-01-11 07:02:37 +0000

    Load mod files

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-document.getElementById('start').onclick = function() {
-  const params = {
-    args: ["-nomaster",  "-game", "KTI", "+map", "kti1"],
-    filesystem: "RAM", //default, can be "IndexedDB" or "LocalStorage" 
-    fullscreen: true,
-  };
-  HLEngine.start(params);
-}
+let zipArrayBuffer;
 
 HLEngine.init({
-  zipElement: document.getElementById('zip'),
-  statusElement: document.getElementById('status'),
   canvas: document.getElementById('canvas'),
   location: 'hl-engine-js/lib'
 });
 
+fetch("KTI.zip")
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => {
+    zipArrayBuffer = arrayBuffer;
+    // enable Start button
+    document.getElementById('start').disabled = false;
+  });
+
+document.getElementById('start').onclick = function() {
+  HLEngine.start({
+    zip: zipArrayBuffer,
+    game: "KTI",
+    map: "kti1",
+    filesystem: "RAM",
+    fullscreen: true,
+  });
+};
